0dba0e36dbb2f68adcd2745baa2590df0955333a
luis-otavio-dias <luis777dias231@gmail.com>
2026-06-21 12:06:24 -0300

    Abre pacotes para visualização e manipulação em javafx

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -3,4 +3,7 @@ module com.luisotaviodias {
     requires javafx.controls;
 
     exports com.luisotaviodias;
+
+    opens com.luisotaviodias.view to javafx.graphics;
+    opens com.luisotaviodias.model to javafx.base;
 }
