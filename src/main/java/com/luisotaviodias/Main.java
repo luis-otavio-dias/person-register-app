8bd8491aa460911de3069f72658733168e905e4c
luis-otavio-dias <luis777dias231@gmail.com>
2026-06-21 12:14:38 -0300

    Adiciona a classe Main para iniciar a aplicação com a interface PersonView

---
+++ src/main/java/com/luisotaviodias/Main.java
@@ -0,0 +1,10 @@
+package com.luisotaviodias;
+
+import com.luisotaviodias.view.PersonView;
+import javafx.application.Application;
+
+public class Main {
+    public static void main(String[] args) {
+        Application.launch(PersonView.class, args);
+    }
+}
